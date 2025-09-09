69bdfd75b1965c0a289309859559167b2706cede
Dhinesh <dhinesh.tech2001@gmail.com>
2025-09-09 10:41:41 +0530

    Font fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:topgrade/utils/constants/fonts.dart';
 
 import 'features/locale/locale.dart';
 import 'features/presentation/bindings/initial_bindings.dart';
 import 'features/presentation/controllers/onboarding_controller.dart';
 import 'features/presentation/routes/routes.dart';
 import 'utils/constants/strings.dart';
-import 'utils/font_loader.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Preload fonts to ensure they work in release builds
-  await FontPreloader.preloadFonts();
-  await FontPreloader.loadCommonWeights();
-  
+
   // TODO: Temporarily commented for development - uncomment when ready
-  // Check if onboarding is completed
+  // Check if onboarding is complete.
   // final bool isOnboardingCompleted = await OnboardingController.isOnboardingCompleted();
-  
   // Always show onboarding during development
+  
   runApp(const MyApp(initialRoute: XRoutes.onboarding));
 }
 
@@ -33,7 +29,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        fontFamily: 'Lexend',
+        fontFamily: XFonts.lexend,
+        fontFamilyFallback: ['Roboto', 'Arial', 'sans-serif'],
       ),
       title: XString.appName,
       getPages: XRoutes.routes,
